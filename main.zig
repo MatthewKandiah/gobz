4aea9168f44aa8561f6c091772e21a495003eb79
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-12-15 20:12:03 +0000

    handle resizing

--- main.zig
+++ main.zig
@@ -3,8 +3,8 @@ const c = @cImport(
     @cInclude("SDL2/SDL.h"),
 );
 
-const WIDTH = 800;
-const HEIGHT = 600;
+const DEFAULT_WIDTH = 800;
+const DEFAULT_HEIGHT = 600;
 
 pub fn main() void {
     const sdl_init = c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_TIMER | c.SDL_INIT_EVENTS);
@@ -16,29 +16,48 @@ pub fn main() void {
         "Gobz",
         c.SDL_WINDOWPOS_UNDEFINED,
         c.SDL_WINDOWPOS_UNDEFINED,
-        WIDTH,
-        HEIGHT,
-        0,//c.SDL_WINDOW_RESIZABLE,
+        DEFAULT_WIDTH,
+        DEFAULT_HEIGHT,
+        c.SDL_WINDOW_RESIZABLE,
     ) orelse @panic("no window");
 
-    const surface: *c.SDL_Surface = c.SDL_GetWindowSurface(window) orelse std.debug.panic("No surface\n", .{});
-    const pixels: [*]u8 = @ptrCast(surface.pixels orelse @panic("No pixels"));
-    const pixels_count = 4 * WIDTH * HEIGHT;
-    const pixels_slice = pixels[0..pixels_count];
-    for (pixels_slice) |*p| {
-        p.* = 122;
-    }
-    if (c.SDL_UpdateWindowSurface(window) < 0) {
-        @panic("Couldn't update window surface");
-    }
-
+    var surface_info = getSurfaceInfo(window);
     var running = true;
     var event: c.SDL_Event = undefined;
     while (running) {
+        // clear screen
+        for (surface_info.bytes) |*p| {
+            p.* = 122;
+        }
+
+        // handle events
         while (c.SDL_PollEvent(@ptrCast(&event)) != 0) {
             if (event.type == c.SDL_QUIT) {
                 running = false;
             }
+            if (event.type == c.SDL_WINDOWEVENT) {
+                surface_info = getSurfaceInfo(window);
+            }
+        }
+
+        if (c.SDL_UpdateWindowSurface(window) < 0) {
+            @panic("Couldn't update window surface");
         }
     }
+}
+
+const Surface = struct {
+    bytes: []u8,
+    width_pixels: usize,
+    height_pixels: usize,
+};
+
+fn getSurfaceInfo(window: *c.SDL_Window) Surface {
+    const surface: *c.SDL_Surface = c.SDL_GetWindowSurface(window) orelse std.debug.panic("No surface\n", .{});
+    const width: usize = @intCast(surface.w);
+    const height: usize = @intCast(surface.h);
+    const pixels: [*]u8 = @ptrCast(surface.pixels orelse @panic("No pixels"));
+    const pixels_count = 4 * width * height;
+    const bytes = pixels[0..pixels_count];
+    return .{ .bytes = bytes, .width_pixels = width, .height_pixels = height };
 }
