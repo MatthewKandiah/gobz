d4aac55cd2b0842b3525bb39afacd9371236bb89
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-01-01 12:15:40 +0000

    fixing basic line drawing

--- src/draw_line.zig
+++ src/draw_line.zig
@@ -16,11 +16,12 @@ pub fn drawLine(start: Pos, end: Pos, pos_buffer: []Pos) []Pos {
         std.debug.assert(@abs(gradient) <= 1);
         var insert_idx: usize = 0;
         var current_y = @as(f64, @floatFromInt(start.y));
+        const going_right = dx > 0;
         while (@as(f64, @floatFromInt(insert_idx)) <= @abs(dx)) {
-            const current_pos = if (dx > 0) Pos{ .x = start.x + insert_idx, .y = @intFromFloat(@trunc(current_y)) } else Pos{ .x = start.x - insert_idx, .y = @intFromFloat(@trunc(current_y)) };
+            const current_pos = if (going_right) Pos{ .x = start.x + insert_idx, .y = @intFromFloat(@trunc(current_y)) } else Pos{ .x = start.x - insert_idx, .y = @intFromFloat(@trunc(current_y)) };
             pos_buffer[insert_idx] = current_pos;
             insert_idx += 1;
-            current_y += gradient;
+            current_y += if (going_right) gradient else -gradient;
         }
         return pos_buffer[0..insert_idx];
     } else {
@@ -30,11 +31,12 @@ pub fn drawLine(start: Pos, end: Pos, pos_buffer: []Pos) []Pos {
         std.debug.assert(@abs(gradient) <= 1);
         var insert_idx: usize = 0;
         var current_x = @as(f64, @floatFromInt(start.x));
+        const going_up = dy > 0;
         while (@as(f64, @floatFromInt(insert_idx)) <= @abs(dy)) {
-            const current_pos = if (dy > 0) Pos{ .x = @intFromFloat(@trunc(current_x)), .y = start.y + insert_idx } else Pos{ .x = @intFromFloat(@trunc(current_x)), .y = start.y - insert_idx };
+            const current_pos = if (going_up) Pos{ .x = @intFromFloat(@trunc(current_x)), .y = start.y + insert_idx } else Pos{ .x = @intFromFloat(@trunc(current_x)), .y = start.y - insert_idx };
             pos_buffer[insert_idx] = current_pos;
             insert_idx += 1;
-            current_x += gradient;
+            current_x += if (going_up) gradient else -gradient;
         }
         return pos_buffer[0..insert_idx];
     }
@@ -106,3 +108,71 @@ test "should draw horizontal line left" {
         result,
     );
 }
+
+test "should draw line up-right" {
+    const start = Pos{ .x = 5, .y = 7 };
+    const end = Pos{ .x = 7, .y = 9 };
+
+    const result = drawLine(start, end, &test_draw_line_buf);
+
+    try std.testing.expectEqualSlices(
+        Pos,
+        &.{ Pos{ .x = 5, .y = 7 }, Pos{ .x = 6, .y = 8 }, Pos{ .x = 7, .y = 9 } },
+        result,
+    );
+}
+
+test "should draw line down-right" {
+    const start = Pos{ .x = 5, .y = 7 };
+    const end = Pos{ .x = 7, .y = 5 };
+
+    const result = drawLine(start, end, &test_draw_line_buf);
+
+    try std.testing.expectEqualSlices(
+        Pos,
+        &.{ Pos{ .x = 5, .y = 7 }, Pos{ .x = 6, .y = 6 }, Pos{ .x = 7, .y = 5 } },
+        result,
+    );
+}
+
+test "should draw line down-left" {
+    const start = Pos{ .x = 5, .y = 7 };
+    const end = Pos{ .x = 3, .y = 5 };
+
+    const result = drawLine(start, end, &test_draw_line_buf);
+
+    try std.testing.expectEqualSlices(
+        Pos,
+        &.{ Pos{ .x = 5, .y = 7 }, Pos{ .x = 4, .y = 6 }, Pos{ .x = 3, .y = 5 } },
+        result,
+    );
+}
+
+test "should draw line up-left" {
+    const start = Pos{ .x = 5, .y = 7 };
+    const end = Pos{ .x = 3, .y = 9 };
+
+    const result = drawLine(start, end, &test_draw_line_buf);
+
+    try std.testing.expectEqualSlices(
+        Pos,
+        &.{ Pos{ .x = 5, .y = 7 }, Pos{ .x = 4, .y = 8 }, Pos{ .x = 3, .y = 9 } },
+        result,
+    );
+}
+
+test "should draw steep line up-right" {}
+
+test "should draw shallow line up-right" {}
+
+test "should draw steep line down-right" {}
+
+test "should draw shallow line down-right" {}
+
+test "should draw steep line down-left" {}
+
+test "should draw shallow line down-left" {}
+
+test "should draw steep line up-left" {}
+
+test "should draw shallow line up-left" {}
